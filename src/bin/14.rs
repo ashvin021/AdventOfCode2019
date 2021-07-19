8549aa7122ef2158aa4e7f168be3d6c4b7fffc88
Ashvin Arsakularatne <ashvin.arsakularatne@gmail.com>
2021-07-19 13:25:07 +0530

    14: complete part 2 of day 14

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,4 +1,7 @@
-use std::collections::{HashMap, VecDeque};
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, VecDeque},
+};
 
 use itertools::Itertools;
 use regex::Regex;
@@ -8,17 +11,17 @@ use aoc2019::*;
 #[derive(Debug)]
 struct Chemical {
     name: String,
-    qty: u32,
+    qty: u64,
 }
 
-fn part01(equations: &HashMap<String, (u32, Vec<Chemical>)>) -> u32 {
+fn ores_needed(equations: &HashMap<String, (u64, Vec<Chemical>)>, qty: u64) -> u64 {
     let mut needed = VecDeque::new();
     let mut inventory = HashMap::new();
     let mut num_ores = 0;
 
     needed.push_back(Chemical {
         name: "FUEL".to_owned(),
-        qty: 1,
+        qty,
     });
 
     loop {
@@ -42,25 +45,20 @@ fn part01(equations: &HashMap<String, (u32, Vec<Chemical>)>) -> u32 {
     num_ores
 }
 
-fn deduct_from_inventory(target: &Chemical, inventory: &mut HashMap<String, u32>) -> u32 {
+fn deduct_from_inventory(target: &Chemical, inventory: &mut HashMap<String, u64>) -> u64 {
     let mut default = 0;
     let available = inventory.get_mut(&target.name).unwrap_or(&mut default);
 
-    if *available >= target.qty {
-        *available -= target.qty;
-        0
-    } else {
-        let required = target.qty - *available;
-        *available = 0;
-        required
-    }
+    let required = target.qty.checked_sub(*available).unwrap_or(0);
+    *available = (*available).checked_sub(target.qty).unwrap_or(0);
+    required
 }
 
 fn lookup_reaction(
     target: &String,
-    qty: u32,
-    equations: &HashMap<String, (u32, Vec<Chemical>)>,
-) -> (Vec<Chemical>, u32) {
+    qty: u64,
+    equations: &HashMap<String, (u64, Vec<Chemical>)>,
+) -> (Vec<Chemical>, u64) {
     let (output_qty, ingredients) = equations.get(target).unwrap();
     let factor = (qty + (output_qty - 1)) / output_qty;
 
@@ -74,7 +72,28 @@ fn lookup_reaction(
     (scaled, (output_qty * factor) - qty)
 }
 
-fn day_14() -> (u32, u32) {
+fn part01(equations: &HashMap<String, (u64, Vec<Chemical>)>) -> u64 {
+    ores_needed(equations, 1)
+}
+
+fn part02(equations: &HashMap<String, (u64, Vec<Chemical>)>, available_ore: u64) -> u64 {
+    let mut lo = available_ore / ores_needed(equations, 1);
+    let mut hi = available_ore;
+    let mut mid = (lo + hi) / 2;
+
+    while lo < hi - 1 {
+        mid = (lo + hi) / 2;
+        match ores_needed(equations, mid).cmp(&available_ore) {
+            Ordering::Less => lo = mid,
+            Ordering::Greater => hi = mid,
+            Ordering::Equal => break,
+        }
+    }
+
+    mid
+}
+
+fn day_14() -> (u64, u64) {
     let input_re = Regex::new("(\\d+) (\\w+),? ").unwrap();
     let output_re = Regex::new("=> (\\d+) (\\w+)").unwrap();
 
@@ -83,12 +102,12 @@ fn day_14() -> (u32, u32) {
             .captures_iter(line)
             .map(|cap| Chemical {
                 name: cap[2].to_owned(),
-                qty: cap[1].parse::<u32>().unwrap(),
+                qty: cap[1].parse::<u64>().unwrap(),
             })
             .collect_vec();
         let output = output_re
             .captures(line)
-            .map(|cap| (cap[1].parse::<u32>().unwrap(), cap[2].to_owned()))
+            .map(|cap| (cap[1].parse::<u64>().unwrap(), cap[2].to_owned()))
             .unwrap();
 
         (output.1, (output.0, input))
@@ -99,10 +118,11 @@ fn day_14() -> (u32, u32) {
         .into_iter()
         .collect();
 
-    println!("{:?}", map);
-
     let p1 = part01(&map);
-    (p1, 0)
+
+    const AVAILABLE_ORE: u64 = 1_000_000_000_000;
+    let p2 = part02(&map, AVAILABLE_ORE);
+    (p1, p2)
 }
 
 timed_main!(1, day_14());
